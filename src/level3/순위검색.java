cb6ec305accd5246a487301ac6a478daa00445c7
aksmf1442 <aksmf1442@naver.com>
2022-11-29 18:33:58 +0900

    feat: 프로그래머스 순위검색 - level2 복습

--- src/level3/순위검색.java
+++ src/level3/순위검색.java
@@ -4,63 +4,87 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class 순위검색 {
 
     public static void main(String[] args) {
         순위검색_Solution solution = new 순위검색_Solution();
+        String[] info = {"java backend junior pizza 150", "python frontend senior chicken 210",
+            "python frontend senior chicken 150", "cpp backend senior pizza 260",
+            "java backend junior chicken 80", "python backend senior chicken 50"};
+        String[] query = {"java and backend and junior and pizza 100",
+            "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250",
+            "- and backend and senior and - 150", "- and - and - and chicken 100",
+            "- and - and - and - 150"};
+        int[] result = solution.solution(info, query);
+        for (int i : result) {
+            System.out.print(i + ",");
+        }
     }
 }
 
 class 순위검색_Solution {
-    static HashMap<String, List<Integer>> map;
 
+    Map<String, List<Integer>> map = new HashMap<>();
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        map = new HashMap<String, List<Integer>>();
 
-        for (int i = 0; i < info.length; i++) {
-            String[] p = info[i].split(" ");
-            makeSentence(p, "", 0);
+        for (String userInfo: info) {
+            dfs("", userInfo.split(" "), 0);
         }
 
-        for (String key : map.keySet())
+        for (String key : map.keySet()) {
             Collections.sort(map.get(key));
+        }
 
         for (int i = 0; i < query.length; i++) {
-            query[i] = query[i].replaceAll(" and ", "");
-            String[] q = query[i].split(" ");
-            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
+            String[] splitQuery = query[i].replaceAll(" and ", "").split(" ");
+            String key = splitQuery[0];
+            int score = Integer.parseInt(splitQuery[1]);
+
+            answer[i] = binarySearch(key, score);
         }
 
         return answer;
     }
 
     private int binarySearch(String key, int score) {
-        List<Integer> list = map.get(key);
-        int start = 0, end = list.size() - 1;
+        if (map.containsKey(key)) {
+            List<Integer> lst = map.get(key);
+            int left = 0;
+            int right = lst.size() - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (list.get(mid) < score)
-                start = mid + 1;
-            else
-                end = mid - 1;
+            if (lst.get(right) < score) {
+                return 0;
+            }
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (lst.get(mid) < score) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return lst.size() - left;
         }
-
-        return list.size() - start;
+        return 0;
     }
 
-    private void makeSentence(String[] p, String str, int cnt) {
-        if (cnt == 4) {
-            if (!map.containsKey(str)) {
-                List<Integer> list = new ArrayList<Integer>();
-                map.put(str, list);
+    private void dfs(String key, String[] userInfo, int depth) {
+        if (depth == 4) {
+            if (map.containsKey(key)) {
+                map.get(key).add(Integer.parseInt(userInfo[4]));
+            } else {
+                List<Integer> lst = new ArrayList<>();
+                lst.add(Integer.parseInt(userInfo[4]));
+                map.put(key, lst);
             }
-            map.get(str).add(Integer.parseInt(p[4]));
             return;
         }
-        makeSentence(p, str + "-", cnt + 1);
-        makeSentence(p, str + p[cnt], cnt + 1);
+
+        dfs(key + "-", userInfo, depth + 1);
+        dfs(key + userInfo[depth], userInfo, depth + 1);
     }
 }

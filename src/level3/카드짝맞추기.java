ff1d8609469af3811067eeb77c8a33daa1277677
aksmf1442 <aksmf1442@naver.com>
2022-12-02 21:50:34 +0900

    feat: 카드 짝 맞추기 init - level3

---
+++ src/level3/카드짝맞추기.java
@@ -0,0 +1,22 @@
+package level3;
+
+public class 카드짝맞추기 {
+
+    public static void main(String[] args) {
+        카드짝맞추기_Solution solution = new 카드짝맞추기_Solution();
+        int[][] board = {{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}};
+        int r = 1;
+        int c = 0;
+
+        System.out.println(solution.solution(board, r, c));
+    }
+}
+
+class 카드짝맞추기_Solution {
+
+    public int solution(int[][] board, int r, int c) {
+        int answer = 0;
+
+        return answer;
+    }
+}

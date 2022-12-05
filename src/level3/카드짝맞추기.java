f934c8cb309ac7c482bf34cd0653fb41035b1fd6
aksmf1442 <aksmf1442@naver.com>
2022-12-05 15:15:02 +0900

    feat: 프로그래머스 카드 짝 맞추기 - level3 복습

--- src/level3/카드짝맞추기.java
+++ src/level3/카드짝맞추기.java
@@ -1,184 +1,217 @@
 package level3;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 
 public class 카드짝맞추기 {
 
     public static void main(String[] args) {
         카드짝맞추기_Solution solution = new 카드짝맞추기_Solution();
-        int[][] board = {{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}};
-        int r = 1;
-        int c = 0;
+        int[][] board = {{3,0,0,2},{0,0,1,0},{0,1,0,0},{2,0,0,3}};
+        int r = 0;
+        int c = 1;
 
         System.out.println(solution.solution(board, r, c));
     }
 }
 
+/**
+ * 1. 뒤집어야 하는 카드의 수를 모두 구한다.
+ * 2. 카드를 뒤집는 순서의 경우의 수를 모두 구한다.(dfs로 모든 경우의 수 찾기)
+ * 3. 짝을 뒤집을 때 어떤 것을 먼저 뒤집을 지에 대한 경우의 수도 고려한다.
+ * -> 1과 1을 뒤집는다고 가정했을 때 첫 번째 1을 먼저 뒤집을 것인지 뒤의 1을 먼저 뒤집을 것인지 두 경우의 수를 모두 구하기.
+ * 4. 해당 행, 열로 이동하는 방법 중에 가장 빠른 방법을 찾는다.(bfs로 최소 이동거리 찾기)
+ */
+
 class 카드짝맞추기_Solution {
 
+    int[] dx = {0, 0, -1, 1};
+    int[] dy = {1, -1, 0, 0};
+
+    int result = Integer.MAX_VALUE;
+    int[] visited;
+    int r;
+    int c;
     int[][] board;
-    int[][] cases;
-    int[] start;
-    int answer;
-    List<Integer> keys;
-    Map<Integer, List<int[]>> map;
+
     public int solution(int[][] board, int r, int c) {
+        this.r = r;
+        this.c = c;
         this.board = board;
-        this.map = new HashMap<>();
-        this.keys = new ArrayList<>();
-        this.start = new int[]{r,c};
-        this.answer = 100;
 
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                if(board[i][j] != 0){
-                    if(!map.containsKey(board[i][j])){
-                        map.put(board[i][j], new ArrayList<>());
-                        keys.add(board[i][j]);
-                    }
-                    map.get(board[i][j]).add(new int[]{i,j});
-                }
+        int flipCardSize = findFlipCardSize(board);
+        visited = new int[flipCardSize];
+
+        List<List<Location>> cardLocations = findCardLocations(board, flipCardSize);
+
+        dfs(new ArrayList<>(), cardLocations, r, c);
+
+        return result;
+    }
+
+    private int findMinDistances(List<List<Location>> candidate) {
+        int x = r;
+        int y = c;
+        int count = 0;
+
+        int[][] board = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                board[i][j] = this.board[i][j];
             }
         }
 
-        permutation(new int[this.keys.size()], 0, new boolean[this.keys.size()]);
-
-        return answer;
-    }
-
-    int check(int[] aCase){
-        int[] cur = this.start;
-
-        int dist = 0;
-        for(int i = 0; i < aCase.length; i++){
-            int dist1 = getDistance(cur, this.map.get(aCase[i]).get(0));
-            int dist2 = getDistance(cur, this.map.get(aCase[i]).get(1));
-            if(dist1 < dist2){
-                dist += dist1+2;
-                dist += getDistance(this.map.get(aCase[i]).get(0), this.map.get(aCase[i]).get(1));
-                cur = this.map.get(aCase[i]).get(1);
-            }else{
-                dist += dist2+2;
-                dist += getDistance(this.map.get(aCase[i]).get(1), this.map.get(aCase[i]).get(0));
-                cur = this.map.get(aCase[i]).get(0);
-            }
-
-            this.board[this.map.get(aCase[i]).get(0)[0]][this.map.get(aCase[i]).get(0)[1]] = 0;
-            this.board[this.map.get(aCase[i]).get(1)[0]][this.map.get(aCase[i]).get(1)[1]] = 0;
-        }
-
-        for(int key: this.map.keySet()){
-            board[this.map.get(key).get(0)[0]][this.map.get(key).get(0)[1]] = key;
-            board[this.map.get(key).get(1)[0]][this.map.get(key).get(1)[1]] = key;
-        }
-
-        return dist;
-    }
-
-    void permutation(int[] aCase, int idx, boolean[] visited){
-        if(idx == aCase.length){
-            int dist = check(aCase);
-            this.answer = dist < answer ? dist : answer;
-        }
-
-        for(int i = 0; i < this.keys.size(); i++){
-            if(!visited[i]){
-                visited[i] = true;
-                aCase[idx] = this.keys.get(i);
-                permutation(aCase, idx+1, visited);
-                visited[i] = false;
-                aCase[idx] = 0;
+        System.out.println("1111");
+        for (List<Location> locations : candidate) {
+            for (int i = 0; i < 2; i++) {
+                count += findMinDistance(board, locations.get(i), x, y) + 1;
+                System.out.println(count);
+                x = locations.get(i).x;
+                y = locations.get(i).y;
+                board[x][y] = 0;
             }
         }
+        if (count == 14) {
+            for (List<Location> locations : candidate) {
+                System.out.println("----");
+                System.out.println(locations.get(0).x);
+                System.out.println(locations.get(0).y);
+                System.out.println("----");
+                System.out.println(locations.get(1).x);
+                System.out.println(locations.get(1).y);
+                System.out.println("----");
+            }
+        }
+
+        return count;
     }
 
-    int getDistance(int[] start, int[] end){
+    private int findMinDistance(int[][] board, Location target, int x, int y) {
         Queue<Node> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[4][4];
+        int[][] visited = new int[4][4];
 
-        visited[start[0]][start[1]] = true;
-        queue.add(new Node(start[0], start[1], 0));
-        while(!queue.isEmpty()){
-            Node cur = queue.poll();
-            int w_ctrl;
-            if(cur.x == end[0] && cur.y == end[1])
-                return cur.dist;
+        visited[x][y] = 1;
+        queue.add(new Node(x, y, 0));
 
-            if(cur.x-1 >= 0) {
-                if(!visited[cur.x-1][cur.y]) {
-                    visited[cur.x - 1][cur.y] = true;
-                    queue.add(new Node(cur.x - 1, cur.y, cur.dist + 1));
-                }
-
-                w_ctrl = cur.x-1;
-                while(w_ctrl > 0 && board[w_ctrl][cur.y] == 0)
-                    w_ctrl--;
-                if(!visited[w_ctrl][cur.y]) {
-                    visited[w_ctrl][cur.y] = true;
-                    queue.add(new Node(w_ctrl, cur.y, cur.dist + 1));
-                }
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (target.x == node.x && target.y == node.y) {
+                return node.distance;
             }
 
-            if(cur.y-1 >= 0) {
-                if(!visited[cur.x][cur.y-1]) {
-                    visited[cur.x][cur.y - 1] = true;
-                    queue.add(new Node(cur.x, cur.y - 1, cur.dist + 1));
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= 4 || ny >= 4 || visited[nx][ny] == 1) {
+                    continue;
                 }
 
-                w_ctrl = cur.y-1;
-                while(w_ctrl > 0 && board[cur.x][w_ctrl] == 0)
-                    w_ctrl--;
-                if(!visited[cur.x][w_ctrl]) {
-                    visited[cur.x][w_ctrl] = true;
-                    queue.add(new Node(cur.x, w_ctrl, cur.dist + 1));
-                }
+                visited[nx][ny] = 1;
+                queue.add(new Node(nx, ny, node.distance + 1));
             }
 
-            if(cur.x+1 < 4) {
-                if(!visited[cur.x+1][cur.y]) {
-                    visited[cur.x + 1][cur.y] = true;
-                    queue.add(new Node(cur.x + 1, cur.y, cur.dist + 1));
+            for (int i = 0; i < 4; i++) {
+                Location current = findCardLocation(board, node.x, node.y, i);
+                if (visited[current.x][current.y] == 1) {
+                    continue;
                 }
-
-                w_ctrl = cur.x+1;
-                while(w_ctrl < 3 && board[w_ctrl][cur.y] == 0)
-                    w_ctrl++;
-                if(!visited[w_ctrl][cur.y]) {
-                    visited[w_ctrl][cur.y] = true;
-                    queue.add(new Node(w_ctrl, cur.y, cur.dist + 1));
-                }
-            }
-
-            if(cur.y+1 < 4 ){
-                if(!visited[cur.x][cur.y+1]) {
-                    visited[cur.x][cur.y + 1] = true;
-                    queue.add(new Node(cur.x, cur.y + 1, cur.dist + 1));
-                }
-
-                w_ctrl = cur.y+1;
-                while(w_ctrl < 3 && board[cur.x][w_ctrl] == 0)
-                    w_ctrl++;
-                if(!visited[cur.x][w_ctrl]) {
-                    visited[cur.x][w_ctrl] = true;
-                    queue.add(new Node(cur.x, w_ctrl, cur.dist + 1));
-                }
-
+                visited[current.x][current.y] = 1;
+                queue.add(new Node(current.x, current.y, node.distance + 1));
             }
         }
-        return 0;
+
+        return 2;
     }
 
-    class Node{
-        int x, y, dist;
-        public Node(int x, int y, int dist){
+    private Location findCardLocation(int[][] board, int x, int y, int i) {
+        x += dx[i];
+        y += dy[i];
+        while (x >= 0 && x < 4 && y >= 0 && y < 4) {
+            if (board[x][y] != 0) {
+                return new Location(x, y);
+            }
+            x += dx[i];
+            y += dy[i];
+        }
+        return new Location(x - dx[i], y - dy[i]);
+    }
+
+    private void dfs(List<List<Location>> candidate, List<List<Location>> cardLocations, int r, int c) {
+        if (candidate.size() == cardLocations.size()) {
+            this.r = r;
+            this.c = c;
+            result = Math.min(result, findMinDistances(candidate));
+            return;
+        }
+
+        for (int i = 0; i < cardLocations.size(); i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            visited[i] = 1;
+            List<Location> locations = cardLocations.get(i);
+
+            candidate.add(cardLocations.get(i));
+            dfs(candidate, cardLocations, r, c);
+            candidate.remove(candidate.size() - 1);
+
+            candidate.add(List.of(locations.get(1), locations.get(0)));
+            dfs(candidate, cardLocations, r, c);
+            candidate.remove(candidate.size() - 1);
+            visited[i] = 0;
+        }
+    }
+
+    private List<List<Location>> findCardLocations(int[][] board, int flipCardSize) {
+        List<List<Location>> cardLocations = new ArrayList<>();
+        for (int i = 0; i < flipCardSize; i++) {
+            cardLocations.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != 0) {
+                    int cardIdx = board[i][j] - 1;
+                    cardLocations.get(cardIdx).add(new Location(i, j));
+                }
+            }
+        }
+        return cardLocations;
+    }
+
+    private int findFlipCardSize(int[][] board) {
+        int size = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0) {
+                    size++;
+                }
+            }
+        }
+        return size / 2;
+    }
+
+    class Location {
+        int x;
+        int y;
+
+        public Location(int x, int y) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+        }
+    }
+
+    class Node {
+        int x;
+        int y;
+        int distance;
+
+        public Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
         }
     }
 }

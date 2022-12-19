477319d27a65399abfed7b2a655d1d1b5b1d2e7c
aksmf1442 <aksmf1442@naver.com>
2022-12-19 21:17:31 +0900

    feat: 프로그래머스: 경주로건설 복습

--- src/level3/경주로건설.java
+++ src/level3/경주로건설.java
@@ -1,5 +1,6 @@
 package level3;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,105 +14,55 @@ public class 경주로건설 {
 }
 
 class 경주로건설_Solution {
-
-    private static int n;
-    private static int[][] map;
-    private static boolean[][] visit;
-
-    private static int[] dx = {-1, 0, 1, 0}; //상우하좌
-    private static int[] dy = {0, 1, 0 ,-1};
-
-    private static int cost = Integer.MAX_VALUE;
+    // 0 - 왼쪽, 1 - 오른쪽, 2 - 위쪽, 3 - 아래쪽
+    int[] dx = {0, 0, -1, 1};
+    int[] dy = {-1, 1, 0, 0};
 
     public int solution(int[][] board) {
-        int answer = 0;
+        int N = board.length;
 
-        //자동차 경주로 건설
-        //nn
-        //0 : 비어있음, 1 : 벽
-        //상하좌우
-        //벽이있는칸 x
-        //직선도로 : 100원
-        //코너 : 500원
-        //최소비용 : bfs
+        int[][][] costArr = new int[N][N][4];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                Arrays.fill(costArr[i][j], Integer.MAX_VALUE);
 
-        //방향
-        //상 : 0
-        //우 : 1
-        //하 : 2
-        //좌 : 3
+        // 오른쪽, 위쪽
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0, 0, 1));
+        queue.add(new Node(0, 0, 0, 3));
 
-        n = board.length;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
 
-        map = board;
-        visit = new boolean[n][n];
+            for (int i = 0; i < 4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                int cost = node.cost + (node.direction == i ? 100 : 600);
 
-        bfs(0,0,-1,0);
-
-        answer = cost;
-
-        return answer;
-    }
-
-    private void bfs(int x, int y, int dir, int price) {
-
-        Queue<Road> q = new LinkedList();
-        q.add(new Road(x,y,dir,price));
-        visit[x][y] = true;
-
-        while (!q.isEmpty()) {
-            Road road = q.remove();
-
-            int qx = road.x;
-            int qy = road.y;
-            int qDir = road.dir;
-            int qPrice = road.cost;
-
-            if (qx == n-1 && qy == n-1) {
-                cost = Math.min(cost, qPrice);
-            }
-
-            for (int i=0; i<dx.length; i++) {
-                int nx = qx + dx[i];
-                int ny = qy + dy[i];
-                int nDir = i;
-                int nPrice = qPrice;
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n || map[nx][ny] == 1) {
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || board[nx][ny] == 1
+                    || costArr[nx][ny][i] <= cost) {
                     continue;
                 }
-
-                if (qDir == -1) {
-                    //처음엔 직선
-                    nPrice += 100;
-                } else if (qDir == nDir) {
-                    //방향 같을 때
-                    nPrice += 100;
-                } else {
-                    //방향 다를 때
-                    nPrice += 600;
-                }
-
-                if (!visit[nx][ny] || map[nx][ny] >= nPrice) {
-                    //방문 x, 이전 값이 더 클 경우 작은값을 넣어준다.
-                    visit[nx][ny] = true;
-                    map[nx][ny] = nPrice;
-                    q.add(new Road(nx, ny, nDir, nPrice));
-                }
-
+                costArr[nx][ny][i] = cost;
+                queue.add(new Node(nx, ny, cost, i));
             }
         }
 
+        return Arrays.stream(costArr[N - 1][N - 1]).min().getAsInt();
     }
 
-    class Road {
-        int x, y, dir, cost;
+    class Node {
+        int x;
+        int y;
+        int cost;
+        int direction;
 
-        Road(int x, int y, int dir, int cost) {
+        public Node(int x, int y, int cost, int direction) {
             this.x = x;
             this.y = y;
-            this.dir = dir;
             this.cost = cost;
+            this.direction = direction;
         }
     }
+
 }

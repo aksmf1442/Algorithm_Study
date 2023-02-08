d3288c91779542a7e86839cea58ee07c2a350f18
aksmf1442 <aksmf1442@naver.com>
2023-02-08 22:40:06 +0900

    feat: 프로그래머스 2XN 타일링

---
+++ src/level2/TwoXN.java
@@ -0,0 +1,21 @@
+package level2;
+
+public class TwoXN {
+    public static void main(String[] args) {
+        TwoXN_Solution solution = new TwoXN_Solution();
+        int n = 6;
+        System.out.println(solution.solution(n));
+    }
+}
+
+class TwoXN_Solution {
+    public int solution(int n) {
+        int[] result = new int[n];
+        result[0] = 1;
+        result[1] = 2;
+        for (int i = 2; i < n; i++) {
+            result[i] = (result[i - 1] + result[i - 2]) % 1000000007;
+        }
+        return result[n - 1];
+    }
+}

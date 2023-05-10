d22a6098f7c8c29e8350fc41d63432c720a09869
aksmf1442 <aksmf1442@naver.com>
2023-05-11 05:45:06 +0900

    feat: 멀쩡한 사각형 복습

---
+++ src/복습/멀쩡한사각형복습.java
@@ -0,0 +1,30 @@
+package 복습;
+
+public class 멀쩡한사각형복습 {
+
+    public static void main(String[] args) {
+        멀쩡한사각형_Solution solution = new 멀쩡한사각형_Solution();
+        int W = 8;
+        int H = 12;
+        System.out.println("solution.solution(W, H) = " + solution.solution(W, H));
+    }
+}
+
+class 멀쩡한사각형_Solution {
+
+    public long solution(int w, int h) {
+        long gcd = findGcd(w, h);
+
+        return ((long) w * h) -  ((w / gcd) + (h / gcd) - 1) * gcd;
+    }
+
+    private int findGcd(int w, int h) {
+        int gcd = 0;
+        int min = Math.min(w, h);
+        for (int i = 1; i <= min; i++) {
+            if (w % i == 0 && h % i == 0)
+                gcd = i;
+        }
+        return gcd;
+    }
+}
